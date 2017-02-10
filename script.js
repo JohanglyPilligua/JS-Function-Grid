9a3cc8218395deb8794e62d3c1e1a82a74dd97ac
Johangly <johanglypilligua1@gmail.com>
2017-02-10 14:16:06 +0000

    i changed stuff

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //Write your function calls below this line
+setColor(0, 0,"red");
+setColor(0, 9,"red");
+setColor(9, 0,"red");
+setColor(9, 9,"red");
+
+
+setColor(1, 2,"Blue");
+setColor(1, 4,"Blue");
+setColor(3, 2,"Blue");
+setColor(4, 3,"Blue");
+setColor(3, 4,"Blue");
+
+setColor(1, 8,"Grey");
+setColor(1, 7,"Grey");
+setColor(2, 7,"Grey");
+setColor(4, 8,"Grey");
+setColor(3, 8,"Grey");
+setColor(4, 7,"Grey");
+
+
+setColor(6, 2,"Yellow");
+setColor(6, 3,"Yellow");
+setColor(6, 4,"Yellow");
+setColor(6, 5,"Yellow");
+setColor(6, 6,"Yellow");
+setColor(6, 7,"Yellow");
+setColor(7, 2,"Yellow");
+setColor(7, 3,"Yellow");
+setColor(7, 4,"Yellow");
+setColor(7, 5,"Yellow");
+setColor(7, 6,"Yellow");
+setColor(7, 7,"Yellow");
+
+setColor(8, 2,"red");
+setColor(8, 3,"red");
+setColor(8, 4,"red");
+setColor(8, 5,"red");
+setColor(8, 6,"red");
+setColor(8, 7,"red");
+
+setColor(9, 2,"blue");
+setColor(9, 3,"blue");
+setColor(9, 4,"blue");
+setColor(9, 5,"blue");
+setColor(9, 6,"blue");
+setColor(9, 7,"blue");
